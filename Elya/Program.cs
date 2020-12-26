81647b65c80fb5d8c0174e2dc8d05d1922062513
Azizjan Ayupov <litpulla@mail.ru>
2020-12-26 16:24:58 +0600

    Add project files.

---
+++ Elya/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+public class Animal
+{
+    public virtual string GetVoice()
+    {
+        return "I have no voice";
+    }
+}
+
+public class Cat : Animal
+{
+    public override string GetVoice()
+    {
+        return "Meow";
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Animal animal = new Animal();
+        Cat cat = new Cat();
+
+        Console.WriteLine($"An animal voice: {animal.GetVoice()}\nA cat voice: {cat.GetVoice()}");
+    }
+}
